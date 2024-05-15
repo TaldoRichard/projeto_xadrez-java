ec30ea476fc9ec8a7f1a70b6fae62a2288febfef
TaldoRichard <richarddegodois@gmail.com>
2024-05-15 18:56:50 -0300

    Classe posição criada

--- src/Aplicação/Programa.java
+++ src/Aplicação/Programa.java
@@ -1,10 +1,21 @@
 package Aplicação;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import Tabuleiro.Posição;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Posição pos = new Posição(3, 5);
+		System.out.println(pos);
+		
+		sc.close();
 	}
 
 }
